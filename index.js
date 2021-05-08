e912620958758c7d205f1d60d58de571211f5027
YuriiDotsenko <yuriidotsenko59@gmail.com>
2021-05-08 17:26:48 +0200

    Переробив повністю дз

--- index.js
+++ index.js
@@ -1,144 +1,104 @@
 import galleryItems from "./gallery-items.js";
 
-const galleryRef = document.querySelector(".gallery");
-const modalRef = document.querySelector("div.lightbox");
+const galleryRef = document.querySelector(".js-gallery");
+const modalRef = document.querySelector(".js-lightbox");
 const btnCloseRef = document.querySelector(
-  'button[data-action="close-lightbox"]'
+  'button[data-action ="close-lightbox"]'
 );
-const imageModalRef = document.querySelector(".lightbox__image");
+const divModalEl = document.querySelector(".lightbox__content");
 const overlayRef = document.querySelector(".lightbox__overlay");
-const leftArrowRef = document.querySelector(".lightbox__arrow-left");
-const rightArrowRef = document.querySelector(".lightbox__arrow-right");
 
-let number = 1;
-let elementById;
-let indexElement = 0;
+const imagesCardMurkup = galleryItems.reduce(
+  (acc, { preview, description, original }) => {
+    return (acc += `<li class="gallery__item">
+  <a class="gallery__link" href="${original}" >
+  <img loading="lazy" class="gallery__image"
+  src="${preview}"
+  alt="${description}"
+  />
+  </a>
+  </li>`);
+  },
+  ""
+);
+galleryRef.innerHTML = imagesCardMurkup;
 
-galleryItems.forEach(({ preview, original, description }, index, i) => {
-  const list = document.createElement("li");
-  const imageElement = document.createElement("img");
-  const imageLink = document.createElement("a");
+const imgEl = document.querySelector(".gallery__image");
 
-  imageElement.setAttribute("src", preview);
-  imageLink.setAttribute("href", original);
-  imageElement.setAttribute("data-source", original);
-  imageElement.setAttribute("alt", description);
-  imageElement.setAttribute("data-id", index);
+galleryRef.addEventListener("click", e);
 
-  galleryRef.append(list);
-  list.append(imageLink);
-  imageLink.append(imageElement);
-
-  list.classList.add("gallery__item");
-  imageElement.classList.add("gallery__image");
-  imageLink.classList.add("gallery__link");
-
-  galleryRef.addEventListener("click", (event) => {
-    if (event.target.nodeName !== "IMG") {
-      return;
+let element;
+function e(eve) {
+  eve.preventDefault();
+  if (eve.target.className !== imgEl.className) {
+    return;
+  }
+  const bigImgEl = eve.target.alt;
+  for (let i = 0; i < galleryItems.length; i++) {
+    if (galleryItems[i].description === bigImgEl) {
+      element = galleryItems[i].original;
     }
+  }
 
-    event.preventDefault();
+  modalRef.classList.add("is-open");
+  divModalEl.innerHTML = `<img class="lightbox__image"
+    src="${element}"
+    alt="${bigImgEl}"
+  />`;
+}
 
-    modalRef.classList.add("is-open");
+btnCloseRef.addEventListener("click", () => {
+  modalRef.classList.remove("is-open");
+});
 
-    imageModalRef.src = event.target.dataset.source;
-    let indexElement = Number(event.target.dataset.id);
+// Очистка пути после закрытия модалки//
 
-    window.addEventListener("keydown", onClickKeydownRightArrow);
-    window.addEventListener("keydown", onClickKeydownLeftArrow);
-    leftArrowRef.addEventListener("click", onClickLeftArrow);
-    rightArrowRef.addEventListener("click", onCLickRightArrow);
+function isOpen() {
+  const divCloseModal = document.querySelector(".lightbox__image");
+  modalRef.classList.remove("is-open");
+  divCloseModal.alt = "";
+  divCloseModal.src = "";
+}
+const closeModalEl = document.querySelector('[data-action="close-lightbox"]');
+closeModalEl.addEventListener("click", isOpen);
 
-    function onClickKeydownRightArrow(e) {
-      if (e.code === "ArrowRight") {
-        const arrayIndexElements = Number(imageElement.dataset.id);
+overlayRef.addEventListener("click", isOpen);
 
-        indexElement += number;
-        // console.log(indexElement)
-        if (indexElement >= galleryItems.length) {
-          indexElement = galleryItems.length - 1;
-        }
+// Управление кнопками //
 
-        if (indexElement === arrayIndexElements) {
-          elementById = imageElement.dataset.source;
-          imageModalRef.src = elementById;
-        }
-      }
-    }
-    function onClickKeydownLeftArrow(e) {
-      if (e.code === "ArrowLeft") {
-        const arrayIndexElements = Number(imageElement.dataset.id);
+document.addEventListener("keydown", (eve) => {
+  const divCloseModal = document.querySelector(".lightbox__image");
 
-        indexElement -= number;
-        // console.log(indexElement)
+  // Кнопка Esc //
+  if (eve.code === "Escape") {
+    isOpen();
+  }
+  if (modalRef.className.includes("is-open")) {
+    const mapDefEl = galleryItems.map((value) => value.original);
+    const indElNum = Number(mapDefEl.indexOf(divCloseModal.src));
 
-        if (indexElement <= 0) {
-          indexElement = 0;
-        }
-        if (indexElement === arrayIndexElements) {
-          elementById = imageElement.dataset.source;
-          imageModalRef.src = elementById;
-        }
-      }
-    }
-    function onClickLeftArrow(e) {
-      if (e.target.nodeName !== "BUTTON") {
+    // Кнопка влево //
+    const mapDelLight = Number(mapDefEl.length) - 1;
+    if (eve.code === "ArrowLeft") {
+      if (eve.target.className === imgEl.className) {
         return;
       }
-      const arrayIndexElements = Number(imageElement.dataset.id);
-
-      indexElement -= number;
-
-      if (indexElement <= 0) {
-        indexElement = 0;
-      }
-      if (indexElement === arrayIndexElements) {
-        elementById = imageElement.dataset.source;
-        imageModalRef.src = elementById;
+      const indLeftEl = indElNum - 1;
+      divCloseModal.src = mapDefEl[indLeftEl];
+      if (indElNum === 0) {
+        divCloseModal.src = mapDefEl[mapDelLight];
       }
     }
-    function onCLickRightArrow(e) {
-      if (e.target.nodeName !== "BUTTON") {
+    // Кнопка вправо //
+    if (eve.code === "ArrowRight" || eve.code === "Space") {
+      if (eve.target.className === imgEl.className) {
         return;
       }
-      const arrayIndexElements = Number(imageElement.dataset.id);
-
-      indexElement += number;
-
-      if (indexElement >= galleryItems.length) {
-        indexElement = galleryItems.length - 1;
+      const indEl = indElNum + 1;
+      divCloseModal.src = mapDefEl[indEl];
+      if (indEl === mapDefEl.length) {
+        divCloseModal.src = mapDefEl[0];
       }
-
-      if (indexElement === arrayIndexElements) {
-        elementById = imageElement.dataset.source;
-        imageModalRef.src = elementById;
-      }
-    }
-  });
-
-  overlayRef.addEventListener("click", onClickOverlay);
-  window.addEventListener("keydown", onClickKeydownEscape);
-  btnCloseRef.addEventListener("click", onClickCloseButton);
-
-  function onClickOverlay(event) {
-    if (event.target.nodeName !== "DIV") {
-      return;
-    }
-    modalRef.classList.remove("is-open");
-    imageModalRef.src = "";
-  }
-  function onClickCloseButton(event) {
-    if (event.target.nodeName !== "BUTTON") {
-      return;
-    }
-    modalRef.classList.remove("is-open");
-    imageModalRef.src = "";
-  }
-  function onClickKeydownEscape(event) {
-    if (event.code === "Escape") {
-      modalRef.classList.remove("is-open");
-      imageModalRef.src = "";
     }
   }
 });
