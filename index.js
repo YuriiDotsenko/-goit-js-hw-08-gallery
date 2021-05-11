f7c5e23c7885b349dfb7f15d6e34e1f8a8be0ffb
YuriiDotsenko <yuriidotsenko59@gmail.com>
2021-05-11 19:46:09 +0200

    Переробив домашку, забрав клавіші в ліво, і вправо.

--- index.js
+++ index.js
@@ -1,101 +1,73 @@
 import galleryItems from "./gallery-items.js";
 
-const galleryRef = document.querySelector(".js-gallery");
-const modalRef = document.querySelector(".js-lightbox");
-const btnCloseRef = document.querySelector(
-  'button[data-action ="close-lightbox"]'
-);
-const divModalEl = document.querySelector(".lightbox__content");
-const overlayRef = document.querySelector(".lightbox__overlay");
-const divCloseModal = document.querySelector(".lightbox__image");
+import galleryList from "./gallery-items.js";
 
-const imagesCardMurkup = galleryItems.reduce(
-  (acc, { preview, description, original }) => {
-    return (acc += `<li class="gallery__item">
-  <a class="gallery__link" href="${original}" >
-  <img loading="lazy" class="gallery__image"
-  src="${preview}"
-  alt="${description}"
-  />
-  </a>
-  </li>`);
-  },
-  ""
-);
-galleryRef.innerHTML = imagesCardMurkup;
+const refs = {
+  gallery: document.querySelector(".js-gallery"),
+  modal: document.querySelector(".js-lightbox"),
+  modalImg: document.querySelector(".lightbox__image"),
+  imgEl: document.querySelector(".gallery__image"),
+  btnRight: document.querySelector(".lightbox__arrow-right"),
+  btnLeft: document.querySelector(".lightbox__arrow-left"),
+};
 
-const imgEl = document.querySelector(".gallery__image");
+const cardsMarkup = createGalleryMarkup(galleryItems);
+refs.gallery.insertAdjacentHTML("beforeend", cardsMarkup);
 
-galleryRef.addEventListener("click", previewClick);
+// Слушатели
+refs.gallery.addEventListener("click", toOpenModal);
+refs.modal.addEventListener("click", closeModal);
 
-let element;
-function previewClick(eve) {
-  eve.preventDefault();
+// Разметка
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+    <a
+    class="gallery__link"
+    href="${original}"
+    >
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </li>`;
+    })
+    .join("");
+}
 
-  if (eve.target.nodeName !== "IMG") {
+// Модальное окно. Ивенты по открытию и закрытию
+function toOpenModal(event) {
+  if (!event.target.classList.contains("gallery__image")) {
     return;
   }
-
-  const bigImgEl = eve.target;
-
-  modalRef.classList.add("is-open");
-  divModalEl.innerHTML = `<img class="lightbox__image"
-    src="${element}"
-    alt="${bigImgEl}"
-  />`;
+  event.preventDefault();
+  refs.modal.classList.add("is-open");
+  refs.modalImg.setAttribute("src", event.target.getAttribute("data-source"));
+  window.addEventListener("keydown", onButtonKey);
 }
 
-btnCloseRef.addEventListener("click", () => {
-  modalRef.classList.remove("is-open");
-});
-
-// Очистка пути после закрытия модалки//
-
-function isClose() {
-  modalRef.classList.remove("is-open");
-  divCloseModal.alt = "";
-  divCloseModal.src = "";
+function toCloseModal() {
+  refs.modal.classList.remove("is-open");
+  refs.modalImg.removeAttribute("src");
+  window.removeEventListener("keydown", onButtonKey);
 }
-const closeModalEl = document.querySelector('[data-action="close-lightbox"]');
-closeModalEl.addEventListener("click", isClose);
 
-overlayRef.addEventListener("click", isClose);
-
-// Управление кнопками //
-
-document.addEventListener("keydown", (eve) => {
-  const divCloseModal = document.querySelector(".lightbox__image");
-
-  // Кнопка Esc //
-  if (eve.code === "Escape") {
-    isClose();
+function closeModal(event) {
+  if (
+    event.target.classList.contains("lightbox__overlay") ||
+    event.target.classList.contains("lightbox__button")
+  ) {
+    toCloseModal();
   }
-  if (modalRef.className.includes("is-open")) {
-    const mapDefEl = galleryItems.map((value) => value.original);
-    const indElNum = Number(mapDefEl.indexOf(divCloseModal.src));
+}
 
-    // Кнопка влево //
-    const mapDelLight = Number(mapDefEl.length) - 1;
-    if (eve.code === "ArrowLeft") {
-      if (eve.target.className === imgEl.className) {
-        return;
-      }
-      const indLeftEl = indElNum - 1;
-      divCloseModal.src = mapDefEl[indLeftEl];
-      if (indElNum === 0) {
-        divCloseModal.src = mapDefEl[mapDelLight];
-      }
-    }
-    // Кнопка вправо //
-    if (eve.code === "ArrowRight") {
-      if (eve.target.className === imgEl.className) {
-        return;
-      }
-      const indEl = indElNum + 1;
-      divCloseModal.src = mapDefEl[indEl];
-      if (indEl === mapDefEl.length) {
-        divCloseModal.src = mapDefEl[0];
-      }
-    }
+function onButtonKey(event) {
+  if (event.code === "Escape") {
+    toCloseModal();
   }
-});
+}
